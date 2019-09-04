239074f2d3180cdc503dbd7c82b59e088c595676
tappasarn <time723@gmail.com>
2019-09-04 20:26:11 +0700

    add python code to call main of cpp program

---
+++ my_python.py
@@ -0,0 +1,38 @@
+"""
+This module is a python wrapper for calling main function of knng.cpp
+We tried to abstract away the implementation of cpp code
+that is the reason why we directly call main function of the cpp program 
+instead of creating a new python main and call cpp APIs
+"""
+
+import sys
+import numpy as np
+import os
+import ctypes
+
+def _prepare_argv(py_argv, cur_dir):
+    # Remove python script path from argv
+    # we don't need it for the cpp program
+    argv = np.array(py_argv)[1 :].tolist()
+
+    #concat working dir path to the first element of argv
+    argv[0] = cur_dir + '/' + argv[0]
+
+    return argv
+
+# load rknng executable
+rknng = ctypes.CDLL('./rknng')
+
+currentDirectory = os.getcwd()
+prepared_argv = _prepare_argv(sys.argv, currentDirectory)
+
+LP_c_char = ctypes.POINTER(ctypes.c_char)
+LP_LP_c_char = ctypes.POINTER(LP_c_char)
+
+argc = len(prepared_argv)
+argv = (LP_c_char * (argc + 1))()
+for i, arg in enumerate(prepared_argv):
+    enc_arg = arg.encode('utf-8')
+    argv[i] = ctypes.create_string_buffer(enc_arg)
+
+rknng.main(argc, argv)
